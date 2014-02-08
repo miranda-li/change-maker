88f392e01c68b68a8a2e29de5e88bc1b91371b1a
Miranda Li <mjl2206@olumbia.edu>
2014-02-08 01:35:09 -0500

    initial commit

---
+++ ChangeMaker.java
@@ -0,0 +1,75 @@
+/*Miranda Li
+UNI: mjl2206
+COMS 3137 Sec 001
+Professor Peter Allen
+*/
+
+import java.util.*;
+
+/**Prints change of a cent value in quarters, dimes, and nickels*/
+public class ChangeMaker {
+
+    public static final int QUARTER = 25;
+    public static final int DIME = 10;
+    public static final int NICKEL = 5;
+    
+    /**Makes Change for given amount.  Calls the other makeChange method.
+       @param amount the amount
+    */
+    public void makeChange( int amount ) {
+	if(( amount % 5 ) != 0 || amount <= 0 )
+	    System.out.println ( amount + " can't be changed." );
+	else {
+	    String s = "Change for " + amount + " = ";
+	    if( amount >= QUARTER )
+		makeChange( amount, QUARTER, s );
+	    if( amount >= DIME )
+		makeChange( amount, DIME, s );
+	    if( amount >= NICKEL )
+		makeChange( amount, NICKEL, s );
+	}
+    }
+
+    /**Recursively prints the change.  The max the the amount you are reducing in this step, and you cannot increase it for the next step (so if you reduce by dimes for this step, you have to reduce by dimes or nickels, not quarters, for the next step).  A
+       @param amount the amount
+       @param max the max to reduce at this step
+       @param s the String to add on to at this step.
+    */
+    public void makeChange( int amount, int max, String s ) {
+	if( amount == 0 ) 
+	    System.out.println(s);
+	else {
+	    String newS = s + " " + max;
+	    int newAmount = amount - max;
+	    if( newAmount == 0 ) //if, after you subtract, the amount is 0, just call makeChange again and you will print the String s.
+		makeChange( 0, max, newS );
+	    if( max == QUARTER ) {
+		if( newAmount >= QUARTER )
+		    makeChange( newAmount, QUARTER, newS );
+		if( newAmount >= DIME )
+		    makeChange( newAmount, DIME, newS );
+		if( newAmount >= NICKEL )
+		    makeChange( newAmount, NICKEL, newS );
+	    }
+	    if( max == DIME ) {
+		if( newAmount >= DIME )
+		    makeChange( newAmount, DIME, newS );
+		if( newAmount >= NICKEL )
+		    makeChange( newAmount, NICKEL, newS );
+	    }
+	    if( max == NICKEL ) {
+		if( newAmount >= NICKEL )
+		    makeChange( newAmount, NICKEL, newS );
+	    }
+	}
+    }
+    
+
+    /**Main method tester: takes user input.
+       @param args the user input to makeChange to.
+    */
+    public static void main( String[] args ) {
+	ChangeMaker a = new ChangeMaker();
+	a.makeChange( Integer.parseInt( args[0] ));
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 change-maker-hw1
 ================
 
-Gives change amount of quarters, dimes, nickels for a cent amount.
+Gives the amount of quarters, dimes, nickels a cent amount can be split into.  HW1 practice on recursion.
+
+To run: in the command line, compile javac ChangeMaker.java, then run java ChangeMaker [value to change].
+
+Anything non-positive or indivisible by 5 cannot be changed (it will print “[number] cannot be changed”).
+
+A practice on recursion:
+
+I created two makeChange() methods; one takes an int (the cent amount you want to change), and one takes an int (the cent amount), a max int, and a String s.  A call to the first makeChange calls the second, passing into the String “Change for [the amount] = “ and the three maxes, 25, 10, and 5 (if the amount to change is greater than the maxes).
+
+The second makeChange() method adds on the max value to the String, then subtracts the max from the amount and calls makeChange() 3 (or fewer) more times, setting the max to 25, 10, and 5 (quarter, dime, and nickel) only if the new amount is greater than the max.  To avoid duplication, the max can only go down; you cannot call a makeChange() with a max of 10, then from there, a max of 25.  This will recursively print out all the combinations of change.
